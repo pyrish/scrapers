0fe0b33ecbb63954122b69b65db0a70f5bdf3d06
mariano.vazquez <mariano.vazquez@live.com>
2018-03-28 11:10:05 -0400

    Added Multiple page scraping functionality

--- monster.py
+++ monster.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
-#from pyvirtualdisplay import Display
 
 import csv
 from bs4 import BeautifulSoup
 import urllib.request
 
+from pyvirtualdisplay import Display
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
 
 def getPageSource(current_page):
 	
-	hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-	req = urllib.request.Request(current_page, headers=hdr)
-	page = urllib.request.urlopen(req)
-	
-	soup = BeautifulSoup(page, "html5lib")
+	driver = webdriver.Firefox()
+	driver.get(current_page)
+	url_code = driver.page_source
+	soup = BeautifulSoup(url_code, "html5lib")
 	return(soup)
+			
+	
+def get_number_pages(soup):
+	links = []
+	for a in soup.find_all('a', class_= 'page-link'):
+		links.append((a['href'])[-1])
+	links = links[:-2]
+	results = [int(i) for i in links]
+	return(results)
 
 
-def find_data(source):
+def find_data(soup):
 	companies = []
 	dates = []
 	fieldnames = ['ID', 'Company', 'Role', 'URL', 'Date']
 		
-	with open('data.csv', 'w', encoding='utf8', newline='') as csvfile:
+	with open('data.csv', 'a', encoding='utf8', newline='') as csvfile:
 		writer = csv.writer(csvfile)
 		writer.writerow(fieldnames)
 
-		for id, b in enumerate(source.find_all('div', class_= 'company'), start=1):
+		for id, b in enumerate(soup.find_all('div', class_= 'company'), start=1):
 			company_name = b.find('a').find('span').get_text()
 			companies.append(company_name)
 			
-		for id, c in enumerate(source.find_all('div', class_= 'job-specs-date'), start=1):
+		for id, c in enumerate(soup.find_all('div', class_= 'job-specs-date'), start=1):
 			date = c.find('p').find('time').get_text()
 			dates.append(date)
 			
-		for id, a in enumerate(source.find_all('div', class_= 'jobTitle'), start=1):
+		for id, a in enumerate(soup.find_all('div', class_= 'jobTitle'), start=1):
 			pholder = a.find('h2').find('a')
 			url = pholder['href']
 			role = pholder.get_text().strip()
@@ -48,19 +57,29 @@ def find_data(source):
 
 if __name__ == '__main__':
   
-	query = input('Enter role to search: ')
-
-	source = getPageSource('https://www.monster.ie/jobs/search/?q='+query+'&where=Dublin__2C-Dublin&sort=dt.rv.di&page=1')
-	find_data(source)
+	display = Display(visible=0, size=(1920, 1080)).start()
 	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	f = open("data.csv", "w")
+	f.truncate()
+	f.close()
+	
+	query = input('Enter role to search: ')
+	
+	page = 'https://www.monster.ie/jobs/search/?q='+query+'&where=Dublin__2C-Dublin&sort=dt.rv.di&page=1'
+	#soup = BeautifulSoup(open('soup.html', encoding ='utf-8'), "html5lib")
+	soup = getPageSource(page)
+	find_data(soup)
+	no_pages = get_number_pages(soup)
+	
+	new_page = 'https://www.monster.ie/jobs/search/?q='+query+'&where=Dublin__2C-Dublin&sort=dt.rv.di&page='
+	
+	for i in no_pages:
+		soup = getPageSource(new_page + str(i))
+		print("Scraping Page number: " + str(i-1))
+		find_data(soup)
+	print("\n")
+	print("Done!!, check the CSV File!")
+
+#		Open thr file to get the source and avoid sending HTTP Requests all the time!!!!
+# 	with open("soup.html", "w", encoding='utf-8') as file:
+# 		file.write(str(soup))
