dc6532807516e68a813f2208d69d40a08d657494
mariano.vazquez <mariano.vazquez@live.com>
2018-04-26 10:44:58 -0400

    OOP Version of Indeed.ie Scraper!

--- class_indeed.py
+++ class_indeed.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import TimeoutException
 
-from pyvirtualdisplay import Display
+#from pyvirtualdisplay import Display
 
 from bs4 import BeautifulSoup
 import urllib.request
@@ -15,32 +15,33 @@ import os
 class IndeedScraper(object):
     def __init__(self, role):
         self.role = role
-        self.url = "https://ie.indeed.com/jobs?as_and={}&radius=25&l=Dublin&fromage=3&limit=50&sort=date".format(role)
-        self.driver = webdriver.Firefox()
-        self.delay = 3
+        self.url = 'https://ie.indeed.com/jobs?q={}&l=Dublin&sort=date&limit=20&fromage=3&radius=25&start=0'.format(role)
+        #self.driver = webdriver.Firefox()
+        #self.delay = 3
 
-    def load_indeed_url(self):
-        self.driver.get(self.url)
-        try:
-            wait = WebDriverWait(self.driver, self.delay)
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "input_text")))
-            print("Page is ready")
-        except TimeoutException:
-            print("Loading took too much time")
+#     def load_indeed_url(self):
+#         self.driver.get(self.url)
+#         try:
+#             wait = WebDriverWait(self.driver, self.delay)
+#             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "input_text")))
+#             print("Page is ready")
+#         except TimeoutException:
+#             print("Loading took too much time")
             
-#     def find_pages(self):
-#         pages = []
-#         html_page = urllib.request.urlopen(self.url)
-#         source = BeautifulSoup(html_page, "html5lib")
-#         base_url = 'https://ie.indeed.com'
-#         for a in source.find_all('div', class_= 'pagination'):
-#           for link in a.find_all('a', href=True):
-#             pages.append(base_url + link['href'])
-#         pages.insert(0, base_url)
-#         pages.pop()
-#         return pages
+    def find_pages(self):
+        pages = []
+        html_page = urllib.request.urlopen(self.url)
+        source = BeautifulSoup(html_page, "html5lib")
+        base_url = 'https://ie.indeed.com'
+        for a in source.find_all('div', class_= 'pagination'):
+          for link in a.find_all('a', href=True):
+            pages.append(base_url + link['href'])
+        pages.insert(0, base_url + '/jobs?q={}&l=Dublin&sort=date&limit=20&fromage=3&radius=25&start=0'.format(self.role))
+        pages.pop()
+        return pages
 
-    def find_info(self):
+      
+    def find_info(self, source):
       l = []
       html_page = urllib.request.urlopen(self.url)
       source = BeautifulSoup(html_page, "html5lib")
@@ -59,15 +60,6 @@ class IndeedScraper(object):
             pass
       return l
     
-#     @staticmethod
-#     def find_complete(self):
-#       # Iterate through the pages
-#       l_main = []
-#       for i in pages[:max_pages]:
-#         html_page = urllib.request.urlopen(i)
-#         source = BeautifulSoup(html_page, "html5lib")
-#         print("Scraping Page number: " + i)
-#         l_main.extend(scraper.find_info(source))
     
 if __name__ == '__main__':
   
@@ -83,18 +75,29 @@ if __name__ == '__main__':
   print('###############################################################')
   print('\n')
 
-  display = Display(visible=0, size=(1920, 1080)).start()
-
+  #display = Display(visible=0, size=(1920, 1080)).start()
+  
   role = str(input("Enter role to search: "))
-  #max_pages = int(input('Enter number of pages to scrape: '))
-
-  scraper = IndeedScraper(role)
-  #scraper.load_indeed_url()
-
-  results = scraper.find_info()
-
-  # Put all results into a DataFrame
-  df = pd.DataFrame(results)
+  
+  scraper = IndeedScraper(role) 
+  pages = scraper.find_pages()
+  print('# {} pages were found from the past 3 days'.format(len(pages)))
+  max_pages = int(input('# Enter number of pages to scrape: '))
+  print('\n')
+  
+  l_main = []
+  cont = 1
+  # Iterate through the pages
+  for i in pages[:max_pages]:
+    html_page = urllib.request.urlopen(i)
+    source = BeautifulSoup(html_page, "html5lib")
+    print("Scraping Page number: " + str(cont))
+    results = scraper.find_info(source)
+    cont +=1
+    l_main.extend(results)
+ 
+   # Put all results into a DataFrame
+  df = pd.DataFrame(l_main)
   df = df[['Date', 'Company', 'Role', 'URL']]
   df=df.dropna()
   df.sort_values(by=['Date'], inplace=True, ascending=False)
